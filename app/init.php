ada307fe729643c5b548cd482a58e3031ca4a5ff
Carlos Ferreira <c@rlos.rocks>
2017-02-19 18:22:09 +0000

    Added app folder

---
+++ app/init.php
@@ -0,0 +1,28 @@
+<?php
+
+// Enable (1) Disable (0) development mode
+define('DEV_MODE', 1); 
+if (DEV_MODE)
+{
+	ini_set('display_errors', DEV_MODE);
+	ini_set('display_startup_errors', DEV_MODE);
+	error_reporting(E_ALL);
+}
+
+// Start the session
+session_start();
+
+// Add the app_config.php file
+require $_SERVER['DOCUMENT_ROOT'] . '../../../config/app_config.php';
+
+// Composer autoloader
+require $_SERVER['DOCUMENT_ROOT'] . '../../vendor/autoload.php';
+
+// Load custom classes
+spl_autoload_register(function ($class_name)
+{
+	include $_SERVER['DOCUMENT_ROOT'] . '../../classes/' . $class_name . '.php';
+});
+
+// Require routing file
+require_once $_SERVER['DOCUMENT_ROOT'] . '/../app/routes.php';
---
+++ app/routes.php
@@ -0,0 +1,48 @@
+<?php
+
+function handleIndexPage()
+{
+	$tpl = Template::create('pages/index.tpl');
+	$tpl->assign('page_title', 'Welcome');
+	$tpl->assign('page_id', 'index');
+	$tpl->assign('social_title', 'Carlos Ferreira - All Things Developer from London');
+	$tpl->assign('social_desc', 'Name is Carlos and I like making things. Check out my website to know what I can do.');
+	$tpl->display();
+}
+
+function handleContactPage()
+{
+	$tpl = Template::create('pages/contact.tpl');
+	$tpl->assign('page_title', 'Get In Touch');
+	$tpl->assign('page_id', 'contact');
+	$tpl->assign('social_title', 'Contact Carlos Ferreira - All Things Developer from London');
+	$tpl->assign('social_desc', 'Name is Carlos and I like making things. Check out my website to know what I can do.');
+	$tpl->display();
+}
+
+
+
+/** 404 Page **/
+function handle404Page()
+{
+	header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
+	$tpl = Template::create('404-splash.tpl');
+	$tpl->assign('page_title', '404 Not Found - Carlos Ferreira - All Things Developer');
+	$tpl->assign('title', '404 - Not Found :(');
+	$tpl->assign('subtitle', 'Oops! Looks like whatever you were looking for can not be found... Please try again later. You will be redirected in 10 seconds...');
+	$tpl->display();
+}
+
+/** Handle Routing **/
+function handleRouting(AltoRouter $router)
+{	
+	$match = $router->match();
+	if ($match && is_callable($match['target']))
+	{
+		call_user_func_array($match['target'], $match['params']);
+	}
+	else
+	{
+		handle404Page();
+	}
+}

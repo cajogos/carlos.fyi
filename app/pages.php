431808d9015b189acd78d73c69ec90ac7c220802
Carlos Ferreira <carlosferreira@outlook.com>
2017-06-18 20:35:45 +0100

    Fixed social titles and desc for bitcoin

--- app/pages.php
+++ app/pages.php
@@ -25,7 +25,7 @@ function handleSkillsPage()
     $tpl = Template::create('pages/skills.tpl');
     $tpl->assign('page_title', 'What I can do');
     $tpl->assign('page_id', 'skills');
-    $tpl->assign('social_title', 'What I Can Do');
+    $tpl->assign('social_title', 'What I Can Do | Carlos Ferreira - All Things Developer from London');
     $tpl->assign('social_desc', 'Name is Carlos and I like making things. Check out my website to know what I can do.');
     $tpl->display();
 }
@@ -43,8 +43,10 @@ function handleTimelinePage()
 function handleBitcoinPage()
 {
     $tpl = Template::create('pages/bitcoin.tpl');
-    $tpl->assign('page_title', 'My Bitcoins');
+    $tpl->assign('page_title', 'My Bitcoin Mining');
     $tpl->assign('page_id', 'bitcoin');
+    $tpl->assign('social_title', 'Bitcoin Mining | Carlos Ferreira - All Things Developer from London');
+    $tpl->assign('social_desc', 'Name is Carlos and I like making things. Check out my website to know what I can do.');
 
     // Bitcoin
     $bitcoin_api = BitcoinAPI::get();

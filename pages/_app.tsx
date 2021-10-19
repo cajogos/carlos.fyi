14af9e65b702c8e9cf6df7c504d73e558f8e0b72
Carlos Ferreira <carlosferreira@outlook.com>
2021-10-19 19:12:36 +0100

    Added Bootstrap and React Icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,9 @@ import type { ReactElement, ReactNode } from 'react';
 import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
 
+import Head from 'next/head';
+
+import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/global.scss';
 
 type NextPageWithLayout = NextPage & { getLayout?: (page: ReactElement) => ReactNode };
@@ -10,5 +13,12 @@ type AppPropsWithLayout = AppProps & { Component: NextPageWithLayout };
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout)
 {
     const getLayout = Component.getLayout || (page => page);
-    return getLayout(<Component {...pageProps} />);
+    return getLayout(
+        <>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import type { ReactElement } from 'react';
 import MainLayout from '../layouts/Main';
+import { FaBeer } from 'react-icons/fa';
 
 export default function Index()
 {
-    return <h1>Carlos.fyi</h1>
+    return <h1>Carlos.fyi <FaBeer /></h1>
 };
 
 Index.getLayout = function getLayout(page: ReactElement)

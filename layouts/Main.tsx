6204d440cb3b6830e73a4a61111794e9a2200fe2
Sam Roberts <sam@nucreative.co.uk>
2021-10-27 17:34:33 +0100

    Make new Navigation

--- layouts/Main.tsx
+++ layouts/Main.tsx
@@ -1,7 +1,4 @@
 import Head from 'next/head';
-
-import { FaBars, FaTimes } from 'react-icons/fa';
-
 import Footer from '../components/Footer';
 import SlideoutNavbar from '../components/SlideoutNavbar';
 import { useState } from 'react';
@@ -12,24 +9,15 @@ type LayoutProps = {
 
 export default function MainLayout({ children }: LayoutProps)
 {
-    const [isNavbarOpen, setIsNavbarOpen] = useState(false)
-
     return (
         <>
             <Head>
                 <link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto+Mono" rel="stylesheet" />
             </Head>
+            <SlideoutNavbar/>
             <div className="container">
                 <div className="row">
-                    {isNavbarOpen &&
-                        <div className="col-3">
-                            <SlideoutNavbar isOpen={isNavbarOpen} />
-                        </div>
-                    }
                     <div className="col">
-                        <button className="toggle-button" onClick={() => setIsNavbarOpen(!isNavbarOpen)}>
-                            {isNavbarOpen ? <FaTimes /> : <FaBars />}
-                        </button>
                         <div className="container">
                             {children}
                             <Footer />
